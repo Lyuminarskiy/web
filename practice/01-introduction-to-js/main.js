e07bf436364e5f83e04c94a40eea99f3db625b63
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-04-02 15:39:40 +0400

    Доработана функция.

--- practice/01-introduction-to-js/main.js
+++ practice/01-introduction-to-js/main.js
@@ -1,13 +1,20 @@
-var start = new Date().getTime();
-var end;
+function wait(timeout = 1000) {
+    var start = new Date().getTime();
+    var end;
 
-// Цикл с длительностью в 1 секунду.
-while ((end = new Date().getTime() - start) < 1000);
+    // Цикл с длительностью в 1 секунду.
+    while ((end = new Date().getTime() - start) < timeout);
+}
 
-// Создать параграф с указанием времени загрузки скрипта.
-var message = document.createElement('p');
-message.style.color = 'green';
-message.innerText = 'Скрипт выполнился за ' + end / 1000 + ' с';
+function showInfo() {
+    // Создать параграф с указанием времени загрузки скрипта.
+    var message = document.createElement('p');
+    message.style.color = 'green';
+    message.innerText = 'Скрипт выполнился за ' + end / 1000 + ' с';
 
-// Добавить параграф на страницу.
-document.body.appendChild(message);
+    // Добавить параграф на страницу.
+    document.body.appendChild(message);
+}
+
+wait();
+showInfo();
