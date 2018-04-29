0075e2a83636bb12f1064e9428c996608f2a9efd
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-04-29 21:48:10 +0400

    Небольшие правки.

--- labs/lab3/example/scripts/components/post.js
+++ labs/lab3/example/scripts/components/post.js
@@ -3,6 +3,20 @@
 import './comment.js';
 
 Vue.component('post-elem', {
+  template: `
+    <article class="post">
+      <h3 class="post-author">{{ author }}</h3>
+      <h2 class="post-title">{{ title }}</h2>
+      <p class="post-body">{{ post.body }}</p>
+      <div class="comments">
+        <template v-if="!!post.comments.length">
+          <h3>{{ commentsNumber }}</h3>
+          <comment-elem v-for="comment in post.comments" :key="comment.id" :comment="comment"></comment-elem>
+        </template>
+        <h3 v-else>no comments</h3>
+      </div>
+    </article>
+  `,
   props: ['post'],
   computed: {
     title() {
@@ -14,19 +28,5 @@ Vue.component('post-elem', {
     commentsNumber() {
       return this.post.comments.length + ' comments:';
     }
-  },
-  template: `
-    <article class="post">
-      <h3 class="post-author">{{ author }}</h3>
-      <h2 class="post-title">{{ title }}</h2>
-      <p class="post-body">{{ post.body }}</p>
-      <div class="comments">
-        <template v-if="post.comments.length > 0">
-          <h3>{{ commentsNumber }}</h3>
-          <comment-elem v-for="comment in post.comments" :key="comment.id" :comment="comment"></comment-elem>
-        </template>
-        <h3 v-else>no comments</h3>
-      </div>
-    </article>
-  `
+  }
 });
