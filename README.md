823bd0ad869ef95fecb4dada251680405fcb3bcb
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-09-25 11:35:53 +0400

    Added basic stucture for VuePress.

---
+++ README.md
@@ -0,0 +1,8 @@
+[![GitHub license](https://img.shields.io/github/license/Vladislav-Lyuminarskiy/Web-course.svg)](https://github.com/Vladislav-Lyuminarskiy/Web-course/blob/master/LICENSE)
+[![Build Status](https://travis-ci.com/Vladislav-Lyuminarskiy/Web-course.svg?branch=master)](https://travis-ci.com/Vladislav-Lyuminarskiy/Web-course)
+
+# Web-программирование
+
+Учебные материалы по курсу "Web-программирование".
+
+[Перейти на сайт](https://vladislav-lyuminarskiy-websites.github.io/Web-course-website/)
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  base: '/Web-course-website/',
+  head: [
+    ['meta', {
+      name: 'author',
+      content: 'Владислав Люминарский'
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#3eaf7c'
+    }],
+    ['link', {
+      rel: 'icon',
+      href: '/icons/icon512.png'
+    }],
+    ['link', {
+      rel: 'manifest',
+      href: '/manifest.webmanifest'
+    }]
+  ],
+  locales: {
+    '/': {
+      lang: 'ru-RU',
+      title: 'Web-программирование',
+      description: 'Учебные материалы по курсу "Web-программирование".'
+    }
+  },
+  themeConfig: {
+    sidebar: [
+      '/',
+      {
+        title: 'HTML+CSS',
+        collapsable: false
+      },
+      {
+        title: 'JavaScript',
+        collapsable: false
+      }
+    ],
+    lastUpdated: 'Последнее обновление',
+    repo: 'Vladislav-Lyuminarskiy/Web-course',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'Редактировать эту страницу',
+    serviceWorker: {
+      updatePopup: {
+        message: "Доступно новое содержимое.",
+        buttonText: "Обновить"
+      }
+    },
+    algolia: {
+      apiKey: '892ad28dc056e1eb225c126678ef1c09',
+      indexName: 'c_vladislav',
+      algoliaOptions: {
+        facetFilters: ["tags:web"]
+      }
+    }
+  },
+  serviceWorker: true,
+  evergreen: true
+}
