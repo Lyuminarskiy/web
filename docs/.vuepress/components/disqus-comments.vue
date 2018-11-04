758da37d6629aad449fd8c83783d202e5d9a63e4
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-11-04 13:18:17 +0400

    - Добавлен компонент комментариев Disqus.
    - Блок комментариев добавлен ко всем практическим занятиям и лабораторным.

---
+++ docs/.vuepress/components/disqus-comments.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="disqus_thread"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    var disqus_config = function() {
+      this.page.url = window.location.origin + "/Web-course-website/";
+      this.page.identifier = window.location.pathname;
+    };
+
+    (function() {
+      var script = window.document.createElement("script");
+      script.src = "https://web-course.disqus.com/embed.js";
+      script.setAttribute("data-timestamp", +new Date());
+
+      (window.document.head || window.document.body).appendChild(script);
+    })();
+  }
+};
+</script>
+
+<style scoped>
+#disqus_thread {
+  margin-top: 5rem;
+}
+</style>
