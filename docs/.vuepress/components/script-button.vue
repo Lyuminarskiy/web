d72d199b0b84149744aeae26dff53d224be62b25
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-11-05 12:01:10 +0400

    - Добавлена кнопка перехода на страницу со сценарием занятия.

---
+++ docs/.vuepress/components/script-button.vue
@@ -0,0 +1,42 @@
+<template>
+  <p class="action">
+    <a href="script.html" class="action-button">Открыть сценарий</a>
+  </p>
+</template>
+
+<style scoped>
+.action {
+  margin: 1.8rem auto;
+  text-align: center;
+  line-height: 1.7;
+}
+
+.action > .action-button {
+  display: inline-block;
+  box-sizing: border-box;
+  padding: 0.8rem 1.6rem;
+  border-bottom: 1px solid #389d70;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  font-weight: 500;
+  color: white;
+  background-color: #3eaf7c;
+  transition: background-color 0.1s ease;
+}
+
+.action > .action-button:hover {
+  text-decoration: none;
+  background-color: #4abf8a;
+}
+
+@media (max-width: 419px) {
+  .action {
+    margin: 1.2rem auto;
+  }
+
+  .action > .action-button {
+    padding: 0.6rem 1.2rem;
+    font-size: 1rem;
+  }
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   base: '/Web-course-website/',
