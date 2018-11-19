6e016265c9737a23b78abcaeabc5415453b3995c
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-11-19 17:21:08 +0400

    Доработка задания 6.3.

--- docs/.vuepress/components/practice/06/task-03.vue
+++ docs/.vuepress/components/practice/06/task-03.vue
@@ -2,25 +2,57 @@
   <div class="page-container">
     <form>
       <fieldset>
-        <legend>Основная информация</legend>
+        <legend>Информация о пользователе</legend>
+        <label>
+          <span>Электронная почта:</span>
+          <input type="email" name="email" placeholder="Введите e-mail"
+                 required>
+        </label>
+        <label>
+          <span>Пароль:</span>
+          <input type="password" name="password" placeholder="Введите пароль"
+                 required>
+        </label>
+      </fieldset>
+      
+      <fieldset>
+        <legend>Персональная информация</legend>
         <label>
           <span>Имя:</span>
-          <input type="text" placeholder="Введите ваше имя" required>
+          <input type="text" name="firstname" placeholder="Введите ваше имя"
+                 required>
         </label>
         <label>
           <span>Фамилия:</span>
-          <input type="text" placeholder="Введите вашу фамилию" required>
+          <input type="text" name="lastname" placeholder="Введите вашу фамилию"
+                 required>
         </label>
         <label>
           <span>Отчество:</span>
-          <input type="text" placeholder="Введите ваше отчество" required>
+          <input type="text" name="patronymic"
+                 placeholder="Введите ваше отчество">
+        </label>
+        <span>Укажите ваш пол:</span>
+        <label>
+          <input type="radio" name="gender" value="male" required>
+          <span>Мужской</span>
+        </label>
+        <label>
+          <input type="radio" name="gender" value="female" required>
+          <span>Женский</span>
         </label>
       </fieldset>
+      
       <fieldset>
         <legend>Дополнительная информация</legend>
         <label>
-          <span>Расскажите о себе:</span>
-          <textarea rows="6"></textarea>
+          <span>О себе:</span>
+          <textarea name="about" rows="6"
+                    placeholder="Расскажите о себе..."></textarea>
+        </label>
+        <label>
+          <span>Любимый цвет:</span>
+          <input type="color" name="color">
         </label>
       </fieldset>
       <button>Отправить</button>
@@ -36,11 +68,13 @@
   
   label {
     display: block;
+    margin: 5px 0;
     text-align: left;
   }
   
-  input {
+  input:not([type="radio"]) {
     box-sizing: border-box;
+    min-height: 2em;
     width: 100%;
     padding: 5px;
   }
@@ -61,6 +95,9 @@
   
   fieldset {
     border: 1px dashed black;
+  }
+  
+  legend {
     text-align: center;
   }
   
