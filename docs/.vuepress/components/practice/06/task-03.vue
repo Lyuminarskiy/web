0cb893bad840d7ca702e24ff62e43dff1a15b1ca
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-11-20 01:25:36 +0400

    Доработка задания 6.3.

--- docs/.vuepress/components/practice/06/task-03.vue
+++ docs/.vuepress/components/practice/06/task-03.vue
@@ -2,7 +2,7 @@
   <div class="page-container">
     <form>
       <fieldset>
-        <legend>Информация о пользователе</legend>
+        <legend>Основная информация</legend>
         <label>
           <span>Электронная почта:</span>
           <input type="email" name="email" placeholder="Введите e-mail"
@@ -11,12 +11,8 @@
         <label>
           <span>Пароль:</span>
           <input type="password" name="password" placeholder="Введите пароль"
-                 required>
+                 minlength="5" maxlength="10" required>
         </label>
-      </fieldset>
-      
-      <fieldset>
-        <legend>Персональная информация</legend>
         <label>
           <span>Имя:</span>
           <input type="text" name="firstname" placeholder="Введите ваше имя"
@@ -24,15 +20,14 @@
         </label>
         <label>
           <span>Фамилия:</span>
-          <input type="text" name="lastname" placeholder="Введите вашу фамилию"
-                 required>
+          <input name="lastname" placeholder="Введите вашу фамилию" required>
         </label>
         <label>
-          <span>Отчество:</span>
-          <input type="text" name="patronymic"
-                 placeholder="Введите ваше отчество">
+          <span>Дата рождения:</span>
+          <input type="date" name="birthdate" min="1980-01-01"
+                 max="2018-12-31" required>
         </label>
-        <span>Укажите ваш пол:</span>
+        <span>Пол:</span>
         <label>
           <input type="radio" name="gender" value="male" required>
           <span>Мужской</span>
@@ -44,25 +39,51 @@
       </fieldset>
       
       <fieldset>
+        <label>
+          <span>Фотография профиля:</span>
+          <input type="file" accept="image/*">
+        </label>
+        <label>
+          <span>Город:</span>
+          <select name="city">
+            <optgroup label="Россия">
+              <option value="Москва">Москва</option>
+              <option value="Санкт-Петербург">Санкт-Петербург</option>
+              <option value="Казань">Казань</option>
+            </optgroup>
+            <optgroup label="США">
+              <option value="Нью-Йорк">Нью-Йорк</option>
+              <option value="Вашингтон">Вашингтон</option>
+              <option value="Даллас">Даллас</option>
+            </optgroup>
+          </select>
+        </label>
+        <label>
+          <span>Адрес личной страницы:</span>
+          <input type="url" name="homepage"
+                 placeholder="Введите адрес домашней страницы">
+        </label>
+        <label>
+          <span>Номер телефона:</span>
+          <input type="tel" name="phonenumber" pattern="[0-9]{10}"
+                 maxlength="10" placeholder="Введите номер телефона">
+        </label>
         <legend>Дополнительная информация</legend>
         <label>
           <span>О себе:</span>
           <textarea name="about" rows="6"
                     placeholder="Расскажите о себе..."></textarea>
         </label>
-        <label>
-          <span>Любимый цвет:</span>
-          <input type="color" name="color">
-        </label>
       </fieldset>
       <button>Отправить</button>
+      <button type="reset">Сбросить</button>
     </form>
   </div>
 </template>
 
 <style scoped>
   .page-container {
-    height: 500px;
+    height: 800px;
     overflow-y: scroll;
   }
   
@@ -72,7 +93,7 @@
     text-align: left;
   }
   
-  input:not([type="radio"]) {
+  input:not([type="radio"]), select {
     box-sizing: border-box;
     min-height: 2em;
     width: 100%;
@@ -83,14 +104,20 @@
     border: 1px solid red;
   }
   
-  input:valid {
-    bodred: 1px solid green;
+  input:invalid + span {
+    color: red;
+  }
+  
+  input:checked + span {
+    text-decoration: underline solid black;
   }
   
   button {
     width: 100%;
     padding: 5px;
-    margin: 10px 0;
+    border: 1px solid black;
+    margin: 5px 0;
+    background: white;
   }
   
   fieldset {
