21b5bc032753d6e47a0355d118d39246d207b9c9
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2019-02-25 13:57:05 +0400

    Обновлён конфигурационный файл VuePress.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,116 +2,37 @@ const path = require("path");
 
 module.exports = {
   head: [
-    [
-      "meta", {
+    ["meta", {
       name: "author",
       content: "Владислав Люминарский"
-    }
-    ],
-    [
-      "meta", {
+    }],
+    ["meta", {
       name: "theme-color",
       content: "#3eaf7c"
-    }
-    ],
-    [
-      "link", {
+    }],
+    ["link", {
       rel: "icon",
       href: "/icons/icon512.png"
-    }
-    ],
-    [
-      "link", {
+    }],
+    ["link", {
       rel: "manifest",
       href: "/manifest.webmanifest"
-    }
-    ],
-    [
-      "link", {
+    }],
+    ["link", {
       rel: "license",
       href: "/LICENSE"
-    }
-    ]
+    }]
   ],
   locales: {
     "/": {
       lang: "ru-RU",
       title: "Web-программирование",
-      description: "Учебные материалы по курсу \"Web-программирование\""
+      description: "Учебные материалы по курсу \"Web-программирование\"."
     }
   },
   themeConfig: {
-    nav: [
-      {
-        text: "О курсе",
-        link: "/about/"
-      },
-      {
-        text: "Практика",
-        link: "/practice/"
-      },
-      {
-        text: "Лабораторные",
-        link: "/labs/"
-      },
-      {
-        text: "Экзамен",
-        link: "/exam/"
-      }
-    ],
-    sidebar: {
-      "/practice/": [
-        "",
-        {
-          title: "HTML и CSS",
-          collapsable: false,
-          children: [
-            "01/",
-            "02/",
-            "03/",
-            "04/",
-            "05/",
-            "06/",
-            "07/",
-            "08/"
-          ]
-        },
-        {
-          title: "JavaScript и Node.js",
-          collapsable: false,
-          children: [
-            "09/",
-            "10/",
-            "11/",
-            "12/",
-            "13/",
-            "14/",
-            "15/"
-          ]
-        },
-        {
-          title: "Стек технологий MEVN",
-          collapsable: false,
-          children: []
-        }
-      ],
-      "/labs/": [
-        "",
-        {
-          title: "Лабораторные работы",
-          collapsable: false,
-          children: [
-            "01/",
-            "02/",
-            "03/"
-          ]
-        }
-      ]
-    },
-    lastUpdated: "Последнее обновление",
-    repo: "Web-courses/Web-course",
     docsDir: "docs",
-    editLinks: true,
+    lastUpdated: "Последнее обновление",
     editLinkText: "Редактировать эту страницу",
     serviceWorker: {
       updatePopup: {
@@ -130,13 +51,81 @@ module.exports = {
       }
     }
   },
+  nav: [
+    {
+      text: "О курсе",
+      link: "/about/"
+    },
+    {
+      text: "Практика",
+      link: "/practice/"
+    },
+    {
+      text: "Лабораторные",
+      link: "/labs/"
+    },
+    {
+      text: "Экзамен",
+      link: "/exam/"
+    }
+  ],
+  sidebar: {
+    "/practice/": [
+      "",
+      {
+        title: "HTML и CSS",
+        collapsable: false,
+        children: [
+          "01/",
+          "02/",
+          "03/",
+          "04/",
+          "05/",
+          "06/",
+          "07/",
+          "08/"
+        ]
+      },
+      {
+        title: "JavaScript и Node.js",
+        collapsable: false,
+        children: [
+          "09/",
+          "10/",
+          "11/",
+          "12/",
+          "13/",
+          "14/",
+          "15/"
+        ]
+      },
+      {
+        title: "Стек технологий MEVN",
+        collapsable: false,
+        children: []
+      }
+    ],
+    "/labs/": [
+      "",
+      {
+        title: "Лабораторные работы",
+        collapsable: false,
+        children: [
+          "01/",
+          "02/",
+          "03/"
+        ]
+      }
+    ]
+  },
+  repo: "Web-courses/Web-course",
+  editLinks: true,
+  evergreen: true,
   configureWebpack: {
     resolve: {
       alias: {
         practice: path.resolve(".", "docs/practice")
       }
     }
-  },
-  serviceWorker: true,
-  evergreen: true
+  }
 };
