42ac7ac714585992144d19dd5391330c4e93c0eb
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2019-03-12 18:30:09 +0400

    Доработка примера для занятия №17.

--- docs/practice/17/assets/02-api/server.js
+++ docs/practice/17/assets/02-api/server.js
@@ -11,7 +11,7 @@ let parser = bodyParser.urlencoded({
 let count = 0;
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/", parser, function(request, response) {
