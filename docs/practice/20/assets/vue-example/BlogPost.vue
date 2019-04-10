f5cf61e7e95d7ff1e405d61501c5094087eccc72
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2019-04-10 10:56:25 +0400

    Добавлен пример для занятия №20.

---
+++ docs/practice/20/assets/vue-example/BlogPost.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="block-post">
+    <button @click="$emit('increase-size')">
+      Увеличить
+    </button>
+    <button @click="$emit('decrease-size')">
+      Уменьшить
+    </button>
+
+    <h2>{{ title }}</h2>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["title"]
+  }
+</script>
+
+<style scoped>
+  .block-post {
+    border: 1px solid black;
+    padding: 5px;
+    margin: 5px;
+  }
+</style>
+
